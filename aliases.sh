87df2e37b3482894ad9c52c2d04d25d7a3050ebb
Joel Lappalainen <Jopi@ren.to>
2017-03-28 17:21:22 +0300

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,19 @@
+# Aliases file.
+#
+# Include in bashrc.
+
+# Aptitude
+alias agi='apt-get install'
+alias agr='apt-get remove'
+alias agc='apt-get cache'
+
+#Finding
+alias findname='find . -name'
+
+#Listing
+alias ll='ls -l'
+alias l='ls -ltra'
+alias lb='ls'
+
+#Grepping
+alias grepr='grep -r'
